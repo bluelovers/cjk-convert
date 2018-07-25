0fecd1c4f759d7cbf4787ba6accc056faa8e345b
bluelovers <codelovers@users.sourceforge.net>
2018-07-26 07:54:51 +0800

    npm publish 1.0.60

--- test/list.ts
+++ test/list.ts
@@ -6,7 +6,7 @@ import { array_unique } from 'array-hyper-unique';
 import { charTableList, textList } from '../lib/zh/table/list';
 import { cn2tw, tw2cn } from '../lib/zh/convert';
 
-let word = `蓋蓋葢`;
+let word = `遊歷,遊历,游历`;
 
 word = array_unique(word.split('')).join('');
 
