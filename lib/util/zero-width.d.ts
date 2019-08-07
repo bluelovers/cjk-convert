c13604ba9772316e475fe46e2efe7079defe645d
bluelovers <codelovers@users.sourceforge.net>
2019-08-08 02:40:25 +0800

    .

--- lib/util/zero-width.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Created by user on 2019/7/26.
- */
-export declare const zeroWidthList: readonly ["󠄀", "‌", "‍", "​", "﻿", "‎", "‏"];
-export declare const zeroWidthRe: RegExp;
-export declare function removeZeroWidth(input: string): string;
-export default removeZeroWidth;
