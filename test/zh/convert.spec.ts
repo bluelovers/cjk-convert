544b3318e6a52adccc3363b103c2a83668eba8cf
bluelovers <codelovers@users.sourceforge.net>
2020-05-23 17:30:58 +0800

    refactor: table_same for tw === cn

--- test/zh/convert.spec.ts
+++ test/zh/convert.spec.ts
@@ -1,11 +1,13 @@
 import { cn2tw, tw2cn } from '../../lib/zh/convert';
+import table_same from '@lazy-cjk/zh-convert-table/test/raw/table_same';
 
 describe(`simple check`, () =>
 {
 
 	([
 		['候', '侯'],
-	] as [string, string][]).forEach((ls) => {
+	] as [string, string][]).forEach((ls) =>
+	{
 
 		test(ls.join('|'), () =>
 		{
@@ -35,3 +37,62 @@ describe(`simple check`, () =>
 	});
 
 })
+
+describe(`known bugs of common table`, () =>
+{
+
+	describe(`tw <=> cn`, () =>
+	{
+
+		Object.entries({
+			"瀰": "㳽",
+			'氣': '气',
+			'淨': '净',
+		} as Record<string, string>).forEach((ls) =>
+		{
+
+			test(ls.join(' <=> '), () =>
+			{
+
+				expect(cn2tw(ls[1])).toStrictEqual(ls[0]);
+				expect(tw2cn(ls[0])).toStrictEqual(ls[1]);
+
+				expect(cn2tw(ls[1], {
+					safe: false,
+				})).toStrictEqual(ls[0]);
+				expect(tw2cn(ls[0], {
+					safe: false,
+				})).toStrictEqual(ls[1]);
+
+			});
+
+		});
+
+	});
+
+	describe(`tw === cn`, () =>
+	{
+
+		table_same.forEach((c) =>
+		{
+
+			test(c, () =>
+			{
+
+				expect(cn2tw(c)).toStrictEqual(c);
+				expect(tw2cn(c)).toStrictEqual(c);
+
+				expect(cn2tw(c, {
+					safe: false,
+				})).toStrictEqual(c);
+				expect(tw2cn(c, {
+					safe: false,
+				})).toStrictEqual(c);
+
+			});
+
+		});
+
+	});
+
+});
