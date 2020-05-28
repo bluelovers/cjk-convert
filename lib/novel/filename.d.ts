dbdccb15d8a6dc8f3d0774f204f5a9ce307565a9
bluelovers <codelovers@users.sourceforge.net>
2020-05-29 05:49:59 +0800

    refactor: @lazy-cjk/novel-filename

--- lib/novel/filename.d.ts
+++ lib/novel/filename.d.ts
@@ -1,13 +1,6 @@
 /**
  * Created by user on 2018/2/17/017.
  */
-export interface IOptions {
-    skip?: string;
-    safe?: boolean;
-}
-export declare function filename(name: string, options?: IOptions): string;
-export declare function word(name: string, options?: IOptions): string;
-export declare function jp(txt: string, options?: IOptions): string;
-export declare function zh(txt: string, options?: IOptions): string;
-declare const _default: typeof import("./filename");
-export default _default;
+export * from '@lazy-cjk/novel-filename';
+import m from '@lazy-cjk/novel-filename';
+export default m;
