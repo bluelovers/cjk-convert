c765702192b34dd89071a8b52f9b06b9a7782c81
bluelovers <codelovers@users.sourceforge.net>
2020-06-05 04:20:20 +0800

    fix: avoid multi re-export bug of typescript

--- lib/jp/index.d.ts
+++ lib/jp/index.d.ts
@@ -1,6 +1,6 @@
 /**
  * Created by user on 2018/2/17/017.
  */
-export { zh2jp, zht2jp, zhs2jp, zhs2zht, zht2zhs, cjk2zhs, jp2zhs, jp2zht, cjk2zht, cjk2jp, IOptions } from './core';
+export { zh2jp, zht2jp, zhs2jp, zhs2zht, zht2zhs, cjk2zhs, jp2zhs, jp2zht, cjk2zht, cjk2jp, IOptions } from '@lazy-cjk/jp-table-convert';
 declare const _default: typeof import(".");
 export default _default;
--- lib/jp/index.ts
+++ lib/jp/index.ts
@@ -2,6 +2,6 @@
  * Created by user on 2018/2/17/017.
  */
 
-export { zh2jp, zht2jp, zhs2jp, zhs2zht, zht2zhs, cjk2zhs, jp2zhs, jp2zht, cjk2zht, cjk2jp, IOptions } from './core';
+export { zh2jp, zht2jp, zhs2jp, zhs2zht, zht2zhs, cjk2zhs, jp2zhs, jp2zht, cjk2zht, cjk2jp, IOptions } from '@lazy-cjk/jp-table-convert';
 
 export default exports as typeof import('./index');
